ce67d911f9ba487b1fb72b54e6c7588c5fb07e65
Vrushabh Dave <Vrushabhdave29@gmail.com>
2021-12-25 12:06:59 +0530

    Delete DailyPlayground directory

--- DailyPlayground/src/CallObjectContext.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { DailyCall } from '@daily-co/react-native-daily-js';
-
-export default React.createContext<DailyCall | null>(null);
--- DailyPlayground/src/useCallObject.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import CallObjectContext from './CallObjectContext';
-
-export function useCallObject() {
-  return useContext(CallObjectContext);
-}
